ea971e646f064365462fa5db642aef57de50bd67
Hans Sagita <hans.sagita@gmail.com>
2020-08-18 11:26:42 +0700

    add garuda hacks demo PWA app using VueJs

---
+++ pwa/src/components/TabList.vue
@@ -0,0 +1,68 @@
+<template>
+  <ul>
+    <li
+      v-for="(item, index) in items"
+      :key="index + 1"
+      :class="{ active: item == selectedCategory }"
+      @click="setSelectedItem(index, item)"
+    >
+      {{ item }}
+    </li>
+  </ul>
+</template>
+
+<script>
+import { mapGetters, mapActions } from "vuex";
+export default {
+  data() {
+    return {
+      selectedItem: 0,
+      items: ["people", "planets", "species"]
+    };
+  },
+  computed: {
+    ...mapGetters(["selectedCategory"])
+  },
+  methods: {
+    ...mapActions(["changeSelectedCategory"]),
+    setSelectedItem(index, name) {
+      this.changeSelectedCategory(name);
+      this.selectedItem = index;
+    }
+  },
+  mounted() {
+    this.changeSelectedCategory("people");
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+ul {
+  font-size: 0;
+  position: relative;
+  padding: 0;
+  width: 100%;
+  margin: 16px auto;
+  display: flex;
+  justify-content: space-around;
+}
+
+li {
+  width: 28%;
+  border-radius: 16px;
+  padding: 4px 8px;
+  background: #f1f1f1;
+  font-size: 16px;
+  text-align: center;
+  color: rgba(0, 0, 0, 0.5);
+  text-transform: uppercase;
+  position: relative;
+  overflow: hidden;
+  cursor: pointer;
+}
+
+.active {
+  background-image: linear-gradient(106deg, #f8a33f, #f37021);
+  color: #fff;
+}
+</style>
---
+++ pwa/src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <TabList />
+    <ListItem />
+  </div>
+</template>
+
+<script>
+import TabList from "@/components/TabList";
+import ListItem from "@/components/ListItem";
+export default {
+  name: "Home",
+  components: {
+    TabList,
+    ListItem
+  }
+};
+</script>
+<style scoped></style>
